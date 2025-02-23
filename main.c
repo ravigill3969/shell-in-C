9ac35a35587e261310b1e3869ceae87ffb8cc3f4
ravigill3969 <gill1234ravi@gmail.com>
2025-02-23 18:28:07 -0500

    f gpt I wrote my own today

--- main.c
+++ main.c
@@ -3,102 +3,53 @@
 #include <string.h>
 #include <unistd.h>
 
-void search_executable(const char *command)
-{
-    char *path_env = getenv("PATH");
-    if (!path_env)
-    {
-        printf("%s: not found\n", command);
-        return;
-    }
-
-    char *path = strdup(path_env);
-    if (!path)
-    {
-        perror("strdup");
-        return;
-    }
-
-    char *dir = strtok(path, ":");
-    while (dir)
-    {
-        char full_path[1024];
-        snprintf(full_path, sizeof(full_path), "%s/%s", dir, command);
-
-        if (access(full_path, X_OK) == 0)
-        {
-            printf("%s\n", full_path);
-            free(path);
-            return;
-        }
-        dir = strtok(NULL, ":");
-    }
-
-    printf("%s: not found\n", command);
-    free(path);
-}
-
-int main()
-{
+int main(int argc, char *argv[]) {
     setbuf(stdout, NULL);
-
-    while (1)
-    {
+    while (1) {
         printf("$ ");
-
-        // Read input
         char input[100];
-        if (!fgets(input, sizeof(input), stdin))
-        {
-            break;
-        }
-        input[strcspn(input, "\n")] = '\0';
+        fgets(input, 100, stdin);
+        input[strlen(input) - 1] = '\0';
 
-        if (strcmp(input, "exit 0") == 0)
-        {
-            break;
-        }
+        if (strcmp(input, "exit 0") != 0) {
+            char *firstPart = strtok(input, " ");
+            char *secondPart = strtok(NULL, "");
 
-        char *firstPart = strtok(input, " ");
-        char *secondPart = strtok(NULL, "");
-
-        if (firstPart == NULL)
-        {
-            continue;
-        }
-
-        if (strcmp(firstPart, "echo") == 0)
-        {
-            if (secondPart)
-            {
+            if (strcmp(firstPart, "echo") == 0) {
                 printf("%s\n", secondPart);
-            }
-        }
-        else if (strcmp(firstPart, "type") == 0)
-        {
-            if (secondPart)
-            {
-
-                if (strcmp(secondPart, "echo") == 0 || strcmp(secondPart, "type") == 0 || strcmp(secondPart, "exit") == 0)
-                {
-                    printf("%s is a shell builtin\n", secondPart);
+            } else if (strcmp(firstPart, "type") == 0) {
+                if (secondPart == NULL) {
+                    printf("type: missing argument\n");
+                } else {
+                    char cmd[256];
+                    snprintf(cmd, sizeof(cmd), "command -v %s > /dev/null 2>&1",
+                             secondPart);
+                    if (system(cmd) != 0) {
+                        printf("%s: not found\n", secondPart);
+                    } else {
+                        if (strcmp(secondPart, "echo") == 0 ||
+                            strcmp(secondPart, "cd") == 0) {
+                            printf("%s is a shell builtin\n", secondPart);
+                        } else {
+                            snprintf(cmd, sizeof(cmd), "command -v %s", secondPart);
+                            FILE *fp = popen(cmd, "r");
+                            if (fp != NULL) {
+                                char path[1024];
+                                if (fgets(path, sizeof(path), fp) != NULL) {
+                                    path[strcspn(path, "\n")] = '\0';
+                                    printf("%s is %s\n", secondPart, path);
+                                }
+                                fclose(fp);
+                            }
+                        }
+                    }
                 }
-                else
-                {
-                    search_executable(secondPart);
-                }
+            } else {
+                printf("%s: command not found \n", input);
             }
-            else
-            {
-                printf("type: missing argument\n");
-            }
-        }
-        else
-        {
-            // Handle unknown command
-            printf("%s: command not found\n", firstPart);
+        } else {
+            break;
         }
     }
-
     return 0;
 }
