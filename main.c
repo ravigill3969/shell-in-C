8354877094de01dd51475e2a661728f90508c139
ravigill3936 <gill1234ravi@gmail.com>
2025-07-26 10:17:48 -0400

    added echo functionality

--- main.c
+++ main.c
@@ -3,6 +3,52 @@
 #include <string.h>
 #include <unistd.h>
 
+void print_visually(const char *str) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        unsigned char ch = str[i];
+        if (ch < 32 || ch == 127) {
+            printf("^%c", ch ^ 64);
+        } else {
+            putchar(ch);
+        }
+    }
+}
+
+int echo(char *input) {
+    int no_newline = 0;
+    int visual = 0;
+
+    char *token = strtok(input, " ");
+    while (token && token[0] == '-') {
+        if (strcmp(token, "-n") == 0)
+            no_newline = 1;
+        else if (strcmp(token, "-v") == 0)
+            visual = 1;
+        else
+            break;
+        token = strtok(NULL, " ");
+    }
+
+    char output[100] = "";
+    if (token) {
+        strcat(output, token);
+        char *next = strtok(NULL, "");
+        if (next) {
+            strcat(output, " ");
+            strcat(output, next);
+        }
+    }
+
+    if (visual)
+        print_visually(output);
+    else
+        printf("%s", output);
+
+    if (!no_newline) printf("\n");
+
+    return 0;
+}
+
 int main(int argc, char *argv[]) {
     setbuf(stdout, NULL);
     while (1) {
@@ -11,45 +57,12 @@ int main(int argc, char *argv[]) {
         fgets(input, 100, stdin);
         input[strlen(input) - 1] = '\0';
 
-        if (strcmp(input, "exit 0") != 0) {
-            char *firstPart = strtok(input, " ");
-            char *secondPart = strtok(NULL, "");
-
-            if (strcmp(firstPart, "echo") == 0) {
-                printf("%s\n", secondPart);
-            } else if (strcmp(firstPart, "type") == 0) {
-                if (secondPart == NULL) {
-                    printf("type: missing argument\n");
-                } else {
-                    char cmd[256];
-                    snprintf(cmd, sizeof(cmd), "command -v %s > /dev/null 2>&1",
-                             secondPart);
-                    if (system(cmd) != 0) {
-                        printf("%s: not found\n", secondPart);
-                    } else {
-                        if (strcmp(secondPart, "echo") == 0 ||
-                            strcmp(secondPart, "cd") == 0) {
-                            printf("%s is a shell builtin\n", secondPart);
-                        } else {
-                            snprintf(cmd, sizeof(cmd), "command -v %s", secondPart);
-                            FILE *fp = popen(cmd, "r");
-                            if (fp != NULL) {
-                                char path[1024];
-                                if (fgets(path, sizeof(path), fp) != NULL) {
-                                    path[strcspn(path, "\n")] = '\0';
-                                    printf("%s is %s\n", secondPart, path);
-                                }
-                                fclose(fp);
-                            }
-                        }
-                    }
-                }
-            } else {
-                printf("%s: command not found \n", input);
-            }
-        } else {
-            break;
+        char *cmd = strtok(input, " ");
+        if (cmd && strcmp(cmd, "echo") == 0) {
+            char *rest = strtok(NULL, "");
+            echo(rest);
         }
     }
+
     return 0;
 }
